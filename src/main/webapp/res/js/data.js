b407db99c3a8f16008f586a4a7ab7a82248fc1ac
Seven235 <14301032@bjtu.edu.cn>
2017-06-05 18:54:14 +0800

    上传文件部分的修改

--- src/main/webapp/res/js/data.js
+++ src/main/webapp/res/js/data.js
@@ -1,4 +1,4 @@
-angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpload){
+angular.module('homeApp').controller('dataCtrl', function($scope, $http, Upload){
 // angular.module('dataApp', []).controller('dataCtrl', function($scope, $http){
 	var dataset1 = new Array(), dataset2 = new Array();
 	var width = $('.data-show-chart').width();
@@ -6,26 +6,26 @@ angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpl
 	var padding = { top: 50, right: 50, bottom: 50, left: 50 };
 	$scope.filterParam = 10;
 	$scope.analyze;
+	$scope.noResult = true;
 
-	$scope.sendFile = function(){
-		var file = $scope.fileToUpload;
-		if(!file){
-	    	console.log(2);
-	    	return;
-	    }
-	    var fd = new FormData();
-		fd.append('file', file);
-		$http.post('/toUploadFile', fd, {
-			transformRequest: angular.identity,
-			headers: { "Content-Type": undefined }
-		}).then(function(req){
-			$scope.analyze = req.data;
-		}, function(error){
-
+	$scope.toSubmit = function() {
+		Upload.upload({
+			url: '/toUploadFile',
+			data: {
+				'patientName': $scope.patientName,
+				'patientDate': $scope.patientDate,
+				'file': $scope.file
+			},
+		}).then(function (resp) {
+			$scope.noResult = false;
+			$scope.drawLine();
+			console.log('Success');
+		}, function (resp) {
+		  console.log('Error status');
 		});
-	 };
+	};
 
-	$scope.reflesh = function(){
+	$scope.drawLine = function() {
 		$http({
 			url: '/upload/Alice/data/out.txt',
 			method: 'GET',
@@ -34,14 +34,14 @@ angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpl
 			let dataview = new DataView(req.data);
 			let ints = new Uint8ClampedArray(req.data.byteLength);
 			for (var i = 0; i < ints.length; i++) {
-                ints[i] = dataview.getUint8(i);
-                dataset1[i] = {
+      	ints[i] = dataview.getUint8(i);
+        dataset1[i] = {
 					x: i,
 					y: ints[i]
 				};
-            }
+      }
 			dataset1 = dataset1.filter(function(d){
-				return ((d.y % 20) == 0);
+				return ((d.x % 10) == 0);
 			});
 			var minEcg = d3.min(dataset1, function(d){
 				return d.y;
@@ -62,9 +62,11 @@ angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpl
 						.attr('width', width + 'px')
 						.attr('height', height + 'px');
 			var xAxis = d3.axisBottom()
-							.scale(xScale);
+							.scale(xScale)
+							.ticks(20);
 			var yAxis = d3.axisLeft()
-							.scale(yScale);
+							.scale(yScale)
+							.ticks(20);
 
 			svg.append('g')
 				.attr('class', 'axis')
@@ -85,13 +87,157 @@ angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpl
 				.attr('class', 'line-path')
 				.attr('transform', 'translate(' + padding.left + ',' + padding.top + ')')
 				.attr('d', linePath(dataset1))
-			  	.attr('fill', 'none')
+			  .attr('fill', 'none')
 				.attr('stroke-width', 1)
-				.attr('stroke', 'green');
+				.attr('stroke', 'rgb(84, 172, 174)');
 
 			svg.append('g')
 				.selectAll('circle')
 				.data(dataset1)
+				.enter()
+				.append('circle')
+				.attr('r', 4)
+				.attr('transform', function(d){
+					return 'translate(' + (xScale(d.x) + padding.left) + ',' + (yScale(d.y) + padding.top) + ')'
+				})
+				.attr('fill', 'rgb(84, 172, 174)')
+				.on('mouseover', function(d, i){
+					d3.select('#data-show-heart-rate .data-show-chart svg g:nth-child(5) rect:nth-child(' + (i + 1) + ')')
+						.attr('display', 'block');
+					d3.select('#data-show-heart-rate .data-show-chart svg g:nth-child(6) text:nth-child(' + (i + 1) + ')')
+						.attr('display', 'block');
+				})
+				.on('mouseout', function(d, i){
+					d3.select('#data-show-heart-rate .data-show-chart svg g:nth-child(5) rect:nth-child(' + (i + 1) + ')')
+						.attr('display', 'none');
+						d3.select('#data-show-heart-rate .data-show-chart svg g:nth-child(6) text:nth-child(' + (i + 1) + ')')
+							.attr('display', 'none');
+				});
+
+			svg.append('g')
+				.selectAll('rect')
+				.data(dataset1)
+				.enter()
+				.append('rect')
+				.attr('height', '40px')
+				.attr('width', '40px')
+				.attr('display', 'none')
+				.attr('transform', function(d){
+					return 'translate(' + (xScale(d.x) + padding.left) + ',' + (yScale(d.y) + padding.top) + ')'
+				})
+				.style('fill', 'white')
+				.style('stroke', 'red');
+
+			svg.append('g')
+				.selectAll('text')
+				.data(dataset1)
+				.enter()
+				.append('text')
+				.attr('height', '40px')
+				.attr('width', '40px')
+				.attr('display', 'none')
+				.attr('transform', function(d){
+					return 'translate(' + (xScale(d.x) + padding.left + 20) + ',' + (yScale(d.y) + padding.top + 20) + ')'
+				})
+				.style('fill', 'white')
+				.style('stroke', 'red')
+				.text(function(d){
+					return 'as';
+				});
+		});
+	};
+
+	$scope.reflesh1 = function(){
+		$http({
+			url: '/upload/Alice/data/out.txt',
+			method: 'GET',
+			responseType: 'arraybuffer'
+		}).then(function(req){
+			let dataview = new DataView(req.data);
+			let ints = new Uint8ClampedArray(req.data.byteLength);
+			for (var i = 0; i < ints.length; i++) {
+      	ints[i] = dataview.getUint8(i);
+        dataset1[i] = {
+					x: i,
+					y: ints[i]
+				};
+      }
+			dataset1 = dataset1.filter(function(d){
+				return ((d.x % 1) == 0);
+			});
+			new Chart(document.getElementById('myChart1').getContext('2d'), {
+				type: 'line',
+				data: {
+					labels: dataset1.map(function(item){
+						return item.x;
+					}),
+					datasets: [
+						{
+							label: '心率',
+							fill: false,
+            	borderColor: '#4bc0c0',
+			        data : dataset1.map(function(item){
+								return item.y;
+							})
+						}
+					]
+				}
+			});
+		})
+	};
+	$scope.reflesh2 = function(){
+		$http({
+			url: '/upload/Alice/data/out.txt',
+			method: 'GET',
+			responseType: 'arraybuffer'
+		}).then(function(req){
+			let dataview = new DataView(req.data);
+			let ints = new Uint8ClampedArray(req.data.byteLength);
+			for (var i = 0; i < ints.length; i++) {
+      	ints[i] = dataview.getUint8(i);
+        dataset2[i] = {
+					x: i,
+					y: ints[i]
+				};
+      }
+			dataset2 = dataset2.filter(function(d){
+				return ((d.x % 1) == 0);
+			});
+			var minEcg = d3.min(dataset2, function(d){
+				return d.y;
+			});
+			var maxEcg = d3.max(dataset2, function(d){
+				return d.y;
+			});
+			var maxMinute = dataset2[dataset2.length - 1].x;
+			var xScale = d3.scaleLinear()
+							.domain([0, maxMinute])
+							.rangeRound([0, width - padding.right - padding.left]);
+			var yScale = d3.scaleLinear()
+							.domain([minEcg, maxEcg])
+							.rangeRound([height - padding.top - padding.bottom, 0]);
+
+			var svg = d3.select('#data-show-poincare .data-show-chart')
+						.append('svg')
+						.attr('width', width + 'px')
+						.attr('height', height + 'px');
+			var xAxis = d3.axisBottom()
+							.scale(xScale);
+			var yAxis = d3.axisLeft()
+							.scale(yScale);
+
+			svg.append('g')
+				.attr('class', 'axis')
+				.attr('transform', 'translate(' + padding.left + ', ' + (height - padding.bottom) + ')')
+				.call(xAxis);
+			svg.append('g')
+				.attr('class', 'axis')
+				.attr('transform', 'translate(' + padding.left + ', ' + padding.top + ')')
+				.call(yAxis);
+
+			svg.append('g')
+				.selectAll('circle')
+				.data(dataset2)
 				.enter()
 				.append('circle')
 				.attr('r', 3)
@@ -101,84 +247,4 @@ angular.module('homeApp').controller('dataCtrl', function($scope, $http, fileUpl
 				.attr('fill', 'green');
 		});
 	};
-
-	$scope.reflesh();
-
-	$(window).resize(function(){
-		width = $('.data-show-chart').width();
-		height = width/2;
-		d3.select('#data-show-heart-rate .data-show-chart svg').remove();
-		var minEcg = d3.min(dataset1, function(d){
-			return d.y;
-		});
-		var maxEcg = d3.max(dataset1, function(d){
-			return d.y;
-		});
-		var maxMinute = dataset1[dataset1.length - 1].x;
-		var xScale = d3.scaleLinear()
-						.domain([0, maxMinute])
-						.rangeRound([0, width - padding.right - padding.left]);
-		var yScale = d3.scaleLinear()
-						.domain([minEcg, maxEcg])
-						.rangeRound([height - padding.top - padding.bottom, 0]);
-
-		var svg = d3.select('#data-show-heart-rate .data-show-chart')
-					.append('svg')
-					.attr('width', width + 'px')
-					.attr('height', height + 'px');
-		var xAxis = d3.axisBottom()
-						.scale(xScale);
-		var yAxis = d3.axisLeft()
-						.scale(yScale);
-
-		svg.append('g')
-			.attr('class', 'axis')
-			.attr('transform', 'translate(' + padding.left + ', ' + (height - padding.bottom) + ')')
-			.call(xAxis);
-		svg.append('g')
-			.attr('class', 'axis')
-			.attr('transform', 'translate(' + padding.left + ', ' + padding.top + ')')
-			.call(yAxis);
-
-		var linePath = d3.line()
-						.x(function(d){ return xScale(d.x); })
-						.y(function(d){ return yScale(d.y); })
-						.curve(d3.curveCatmullRom.alpha(0.5));
-
-		svg.append('g')
-			.append('path')
-			.attr('class', 'line-path')
-			.attr('transform', 'translate(' + padding.left + ',' + padding.top + ')')
-			.attr('d', linePath(dataset1))
-		  	.attr('fill', 'none')
-			.attr('stroke-width', 1)
-			.attr('stroke', 'green');
-
-		svg.append('g')
-			.selectAll('circle')
-			.data(dataset1)
-			.enter()
-			.append('circle')
-			.attr('r', 3)
-			.attr('transform', function(d){
-			return 'translate(' + (xScale(d.x) + padding.left) + ',' + (yScale(d.y) + padding.top) + ')'
-			})
-			.attr('fill', 'green');
-	});
-}).directive( "fileModel", function( $parse ){
-  return {
-    restrict: "A",
-    link: function( scope, element, attrs ){
-      var model = $parse( attrs.fileModel );
-      var modelSetter = model.assign;
-
-      element.bind( "change", function(){
-        scope.$apply( function(){
-          modelSetter( scope, element[0].files[0] );
-        } );
-      } );
-    }
-  }
-}).service( "fileUpload", function( $http ){
-	this
 });
